b05e6d611b5417a7997bdcdcad4ca11a86f1fbc5
Steven Starwalt <steven.starwalt@yahoo.com>
2018-05-17 14:53:42 -0700

    added the initial files and code.

--- imager/src/App.js
+++ imager/src/App.js
@@ -1,21 +1,49 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import superagent from 'superagent'
+import Navbar from 'BrowserRouter/Navbar/navbar'
+import Landing from 'BrowserRouter/Landing/landing'
+import { BrowserRouter, Route } from 'react-router-dom'
 
-class App extends Component {
+export default class App extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      token: undefined,
+    }
+    this.signin = this.signin.bind(this)
+    this.signup = this.signup.bind(this)
+  }
+
+  signin(credentials) {
+    let { username, password } = credentials
+    return superagent.post('http://localhost:8000/api/v1/login')
+    .send({ username, password })
+    .then(res => this.setState({ token: res.body.token }))
+    .then(() => localStorage.setItem('token', JSON.stringify(this.state.token)))
+    .catch(console.error)
+  }
+
+  signup(credentials) {
+    let { username, password, email } = credentials
+
+    return superagent.post('http://localhost:8000/api/v1/user/')
+      .send({ username, password, email })
+      .then(() => window.location.pathname = '/welcome/signin')
+      .catch(console.error)
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <BrowserRouter>
+          <React.Fragment>
+            <Navbar />
+            <Route path="/welcome/:auth" component={(props) =>
+                <Landing {...props} signin={this.signin} signup={this.signup} />} />
+          </React.Fragment>
+        </BrowserRouter>
       </div>
-    );
+    )
   }
 }
-
-export default App;
